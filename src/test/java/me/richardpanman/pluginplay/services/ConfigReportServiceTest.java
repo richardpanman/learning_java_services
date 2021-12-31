5f72d29effd76ddec0e70876115e08ccc9178d14
Richard Panman <rpanman@sonatype.com>
2021-12-31 16:00:55 +0000

    Improved test coverage

--- src/test/java/me/richardpanman/pluginplay/services/ConfigReportServiceTest.java
+++ src/test/java/me/richardpanman/pluginplay/services/ConfigReportServiceTest.java
@@ -1,13 +1,27 @@
 package me.richardpanman.pluginplay.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ConfigReportServiceTest {
+    ConfigReportService configReport;
+
+    @BeforeEach
+    void setup() {
+        configReport = new ConfigReportService();
+    }
+
     @Test
     void testGetString() {
-        ConfigReportService configReport = new ConfigReportService();
         assertEquals("ConfigReportService", configReport.getString());
+    }
+
+    @Test
+    void testMissingPropertiesFile() {
+        configReport.setPropertiesFile("unknown.properties.file");
+        assertNull(configReport.getString());
     }
 }

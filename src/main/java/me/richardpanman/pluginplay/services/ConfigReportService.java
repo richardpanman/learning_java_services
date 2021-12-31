f1ab0e493d141446d484640955d81aac15f7de6b
Richard Panman <rpanman@sonatype.com>
2021-12-31 15:58:55 +0000

    Improved test coverage

--- src/main/java/me/richardpanman/pluginplay/services/ConfigReportService.java
+++ src/main/java/me/richardpanman/pluginplay/services/ConfigReportService.java
@@ -1,5 +1,7 @@
 package me.richardpanman.pluginplay.services;
 
+import lombok.Setter;
+
 import org.apache.commons.configuration2.PropertiesConfiguration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 
@@ -7,11 +9,14 @@ import java.io.File;
 
 /** ReportServiceB which doesn't use an external service. */
 public class ConfigReportService implements ReportService {
+
+    @Setter private String propertiesFile = "user.properties";
+
     @Override
     public String getString() {
         Configurations configs = new Configurations();
         try {
-            PropertiesConfiguration config = configs.properties(new File("user.properties"));
+            PropertiesConfiguration config = configs.properties(new File(propertiesFile));
             return config.getString("reportString");
         } catch (Exception e) {
             return null;
